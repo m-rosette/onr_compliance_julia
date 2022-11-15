c60a39a83419609262db3d56a3ad5f326167b920
Marcus Rosette <rosettem@oregonstate.edu>
2022-11-15 10:37:23 -0800

    adding urdf for bravo_5 arm from blueprintlabs

---
+++ examples/urdf_visualizer.jl
@@ -0,0 +1,20 @@
+using MeshCat
+using RigidBodyDynamics
+using MeshCatMechanisms
+
+vis = Visualizer()
+open(vis) # open the visulaizer in a separate window/tab
+# render(vis) # render the visualizer here inside the script
+
+urdf = "C:/Users/marcu/OneDrive/Documents/GitHub/Julia_sim_test/test2.urdf"
+
+robot = parse_urdf(urdf)
+
+mvis = MechanismVisualizer(robot, URDFVisuals(urdf), vis)
+# set_configuration!(mvis, [0.0, 0.0])
+
+# state = MechanismState(robot, randn(2), randn(2))
+# t, q, v = simulate(state, 5.0);
+
+animation = Animation(mvis, t, q)
+setanimation!(mvis, animation)
