7b1b4ac97c6ad2467edc774bde580e03281afe32
Marcus Rosette <rosettem@oregonstate.edu>
2023-01-19 11:37:34 -0800

    implemented batch file saving in bravo_angle_gen_2

--- test/load_robot.m
+++ test/load_robot.m
@@ -14,12 +14,12 @@ bravo = importrobot('bravo7.urdf', DataFormat='column');
 
 
 % Generate Trajectory and Check for Collisions
-startConfig = [pi, 0, pi, 0, pi, 0, 0, 0]'; % For bravo arm 
+startConfig = [pi, pi/2, pi, 0, pi, 0]'; % For bravo arm 
 % startConfig = [0 -pi/4 pi 3*pi/2 0 -pi/2 pi/8]'; % For iiwa arm 
 
 % startConfig = [pi/2, pi/2]'; % For jaws
     
-[is_self_collide, self_sep_dist, self_wit_pts] = checkCollision(bravo, startConfig, 'SkippedSelfCollisions', 'parent');
+is_self_collide = checkCollision(bravo, startConfig, 'SkippedSelfCollisions', 'parent');
 % If the two geometries are in collision, is_self_collide is 1. Otherwise, the value is 0
 
 show(bravo, startConfig,'visuals','on','collision','off')
--- test/three_link_arm.m
+++ test/three_link_arm.m
@@ -37,22 +37,22 @@ final_config = [0 5*pi/6 5*pi/6]';
 
 num_samples = 10;
 traj = trapveltraj([initial_config final_config], num_samples, 'EndTime', 3);
-% 
-% collision_check = false(num_samples, 1); % Initialize array with no collisions
-% collision_traj = [];
-% collision_free_traj = [];
-% 
-% for i = 1:num_samples
-%     collision_check(i) = checkCollision(robot, traj(:, i), 'Exhaustive', 'on', 'SkippedSelfCollisions','parent');
-%     if collision_check(i)
-%         collision_traj(:, end+1) = traj(:, i);
-%     else
-%         collision_free_traj(:, end+1) = traj(:, i);
-% 
-%     end
-% end
 
-% disp(collision_free_traj)
+collision_check = false(num_samples, 1); % Initialize array with no collisions
+collision_traj = [];
+collision_free_traj = [];
+
+for i = 1:num_samples
+    collision_check(i) = checkCollision(robot, traj(:, i), 'Exhaustive', 'on', 'SkippedSelfCollisions','parent');
+    if collision_check(i)
+        collision_traj(:, end+1) = traj(:, i);
+    else
+        collision_free_traj(:, end+1) = traj(:, i);
+
+    end
+end
+
+disp(collision_free_traj)
 
 
 
