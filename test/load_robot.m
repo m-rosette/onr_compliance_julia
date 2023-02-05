47441a18f2a4b90581c64a9681ae702dcee69238
Marcus Rosette <rosettem@oregonstate.edu>
2023-02-04 16:49:20 -0800

    playing with COM calculations

--- test/load_robot.m
+++ test/load_robot.m
@@ -8,17 +8,21 @@ clc
 % Create Robot Representation
 bravo = importrobot('bravo7_planar.urdf', DataFormat='column');
 % bravo = importrobot('base_joint.urdf', DataFormat='column');
+
 % Generate Trajectory and Check for Collisions
-% startConfig = [pi/2, pi, pi]';
-% startConfig = 0;
+config = [pi/2; pi; pi];
+
 % If the two geometries are in collision, is_self_collide is 1. Otherwise, the value is 0
 % is_self_collide = checkCollision(bravo, startConfig, 'SkippedSelfCollisions', 'parent');
 
-% Display the URDF
-show(bravo, 'visuals','on','collision','off')
+% Get COM location and Jacobian
+[com_location, com_jacobian] = centerOfMass(bravo, config);
 
-[com_location, com_jacobian] = centerOfMass(bravo);
-% massMatrix(bravo, startConfig);
+% Display the URDF
+show(bravo, config, 'visuals','on','collision','off')
+hold on
+scatter3(com_location(1), com_location(2), com_location(3), 10000, ".")
+
 
 
 
