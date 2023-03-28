8dd53d4a0d4a07c7fa638a1d4eb525f37343e1ca
Marcus Rosette <rosettem@oregonstate.edu>
2023-03-28 16:24:25 -0700

    adding in all other masses/buoyancy to the model

--- src/simple_pitch/test_urdf_modify.jl
+++ src/simple_pitch/test_urdf_modify.jl
@@ -12,8 +12,8 @@ using onr_compliance_julia
 
 include("FrameSetup_simple.jl")
 include("HydroCalc_simple.jl")
-include("SimWExt.jl")
-include("SimpleController.jl")
+include("SimWExt_simple.jl")
+# include("SimpleController.jl")
 
 println("Libraries imported.")
 
@@ -41,12 +41,17 @@ base_frame = root_frame(mechanism_bravo_vehicle)
 # ----------------------------------------------------------
 #                 COM and COB Frame Setup
 # ----------------------------------------------------------
-frame_names_cob = ["vehicle_cob", "body1_1013_cob", "body2_1026_cob", "body3_1023_cob", "jaw_cob"]
-frame_names_com = ["vehicle_com", "body1_1013_com", "body2_1026_com", "body3_1023_com", "jaw_com"]
+# frame_names_cob = ["vehicle_cob", "body1_1013_cob", "body2_1026_cob", "body3_1023_cob", "jaw_cob"]
+# frame_names_com = ["vehicle_com", "body1_1013_com", "body2_1026_com", "body3_1023_com", "jaw_com"]
+frame_names_cob = ["vehicle_cob", "0_cob", "1_cob", "2_cob", "3_cob", "4_cob", "5_cob", "6_cob", "7_cob",]
+frame_names_com = ["vehicle_com", "0_com", "1_com", "2_com", "3_com", "4_com", "5_com", "6_com", "7_com",]
 # Assume default frame = COM 
 # TODO: Verify the location of the COB and COM on vehicle body
-cob_vecs = [SVector{3, Float64}([0.0, 0.0, 0.02]), SVector{3, Float64}([0.033, -0.043, -0.07]), SVector{3, Float64}([0.020, 0.012, -0.140]), SVector{3, Float64}([0.033, -0.038, -0.008]), SVector{3, Float64}([0.0, 0.0, 0.0])]
-com_vecs = [SVector{3, Float64}([0.0, 0.0, 0.0]), SVector{3, Float64}([0.022, -.029, 0.001]), SVector{3, Float64}([0.017, -0.026, -0.002]), SVector{3, Float64}([0.020, -0.024, 0.001]), SVector{3, Float64}([0.0, 0.0, 0.0])]
+# cob_vecs = [SVector{3, Float64}([0.0, 0.0, 0.02]), SVector{3, Float64}([0.033, -0.043, -0.07]), SVector{3, Float64}([0.020, 0.012, -0.140]), SVector{3, Float64}([0.033, -0.038, -0.008]), SVector{3, Float64}([0.0, 0.0, 0.0])]
+# com_vecs = [SVector{3, Float64}([0.0, 0.0, 0.0]), SVector{3, Float64}([0.022, -.029, 0.001]), SVector{3, Float64}([0.017, -0.026, -0.002]), SVector{3, Float64}([0.020, -0.024, 0.001]), SVector{3, Float64}([0.0, 0.0, 0.0])]
+cob_vecs = [SVector{3, Float64}([0.0, 0.0, 0.02]), SVector{3, Float64}([-0.018, -0.003, -0.003]), SVector{3, Float64}([0.027, -0.011, 0.092]), SVector{3, Float64}([0.145, 0.035, -0.001]), SVector{3, Float64}([0.033, -0.043, -0.07]), SVector{3, Float64}([0.020, 0.012, -0.140]), SVector{3, Float64}([0.033, -0.038, -0.008]), SVector{3, Float64}([0.0, 0.0, -0.152]), SVector{3, Float64}([0.028, 0.001, 0.0])]
+com_vecs = [SVector{3, Float64}([0.0, 0.0, 0.0]), SVector{3, Float64}([-0.018, -.004, -0.001]), SVector{3, Float64}([0.017, -.007, 0.057]), SVector{3, Float64}([0.117, 0.015, 0.006]), SVector{3, Float64}([0.022, -.029, 0.001]), SVector{3, Float64}([0.018, 0.006, -0.117]), SVector{3, Float64}([0.020, -0.024, 0.001]), SVector{3, Float64}([0, 0, -0.128]), SVector{3, Float64}([0.028, -0.001, 0.0])]
+
 cob_frames = []
 com_frames = []
 
@@ -62,7 +67,7 @@ setup_frames!(mechanism_bravo_vehicle, frame_names_cob, frame_names_com, cob_vec
 # KEEP ARM BASE VALUES AT END OF LIST for HydroCalc
 rho = 997
 # TODO: Need to verify vehicle volume calc
-volumes = [60 / (.001*rho), .47, .51, .43, .72] # vehicle, shoulder, ua, elbow, wrist, jaw, armbase
+volumes = [60 / (.001*rho), 0.60, 1.94, 0.47, 0.51, 0.43, 0.48, 0.16, 0.72] # vehicle, ........, armbase
 buoy_force_mags = volumes * rho * 9.81 * .001
 buoy_lin_forces = []
 for mag in buoy_force_mags
@@ -71,7 +76,7 @@ for mag in buoy_force_mags
 end
 
 # TODO: Need to verify vehicle mass
-masses = [60, 1.14, 1.14, 1.03, 1.25]
+masses = [60, 1.55, 1.98, 1.14, 1.14, 1.03, 1.04, 0.47, 1.25] # vehicle, ........, armbase
 grav_forces = masses * 9.81
 grav_lin_forces = []
 for f_g in grav_forces
